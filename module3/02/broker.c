785a906de92e57453ff29f68e4374bb613f9b2e8
danil <kornachenkodanil@gmail.com>
2026-08-06 01:24:41 +0700

    module3 lab02 : complete

--- module3/02/broker.c
+++ module3/02/broker.c
@@ -1,11 +1,13 @@
 #include "broker.h"
+
+#include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <string.h>
 
 // список подписчиков
 sub* subs = NULL;
@@ -21,12 +23,16 @@ int capacity_sms = 0;
 
 int queue_id = -1;
 
-static void append_subscribe(int pid, char* topic)
-{
-  for (int i = 0; i < capacity_subs; i++)
-  {
-    if (subs[i].pid == pid && strcmp(subs[i].topic, topic))
-      return;
+static volatile sig_atomic_t running = 1;
+
+static void signal_handler(int sig) {
+  (void)sig;
+  running = 0;
+}
+
+static void append_subscribe(int pid, char* topic) {
+  for (int i = 0; i < capacity_subs; i++) {
+    if (subs[i].pid == pid && strcmp(subs[i].topic, topic) == 0) return;
   }
 
   capacity_subs++;
@@ -37,12 +43,9 @@ static void append_subscribe(int pid, char* topic)
   subs[capacity_subs - 1].topic = strdup(topic);
 }
 
-static void remove_subscribe(int pid, const char* topic)
-{
-  for (int i = 0; i < capacity_subs; i++)
-  {
-    if (subs[i].pid == pid && strcmp(subs[i].topic, topic) == 0)
-    {
+static void remove_subscribe(int pid, const char* topic) {
+  for (int i = 0; i < capacity_subs; i++) {
+    if (subs[i].pid == pid && strcmp(subs[i].topic, topic) == 0) {
       free(subs[i].topic);
       subs[i] = subs[capacity_subs - 1];
       capacity_subs--;
@@ -51,10 +54,8 @@ static void remove_subscribe(int pid, const char* topic)
   }
 }
 
-static void append_publisher(int pid)
-{
-  for (int i = 0; i < capacity_pubs; i++)
-  {
+static void append_publisher(int pid) {
+  for (int i = 0; i < capacity_pubs; i++) {
     if (pubs[i].pid == pid) return;
   }
 
@@ -65,31 +66,28 @@ static void append_publisher(int pid)
   pubs[capacity_pubs - 1].pid = pid;
 }
 
-static void append_message(char* topic, char* message)
-{
+static void append_message(char* topic, char* message) {
   capacity_sms++;
-  sms = realloc(sms, capacity_sms);
+  sms = realloc(sms, capacity_sms * sizeof(sm));
 
   sms[capacity_sms - 1].topic = strdup(topic);
   sms[capacity_sms - 1].message = strdup(message);
 }
 
-static void send_to_subscriber(int pid, const char* text)
-{
+static void send_to_subscriber(int pid, const char* text) {
   message msg;
   msg.type = pid;
-  strcpy(msg.text, text);
+  strncpy(msg.text, text, sizeof(msg.text) - 1);
+  msg.text[sizeof(msg.text) - 1] = '\0';
 
-  if (msgsnd(queue_id, &msg, sizeof(msg.text), 0) == -1)
-  {
+  if (msgsnd(queue_id, &msg, sizeof(msg.text), 0) == -1) {
     perror("msgsnd to subscriber ");
   }
 }
 
-static void parse_pubs(char* text)
-{
+static void parse_pubs(char* text) {
   char buffer[100];
-  strcpy(buffer, text);
+  snprintf(buffer, sizeof(buffer), "%s", text);
 
   char* token = strtok(buffer, ",");
   if (!token || strcmp(token, "send") != 0) return;
@@ -105,19 +103,16 @@ static void parse_pubs(char* text)
 
   append_message(topic, text);
 
-  for (int i = 0; i < capacity_subs; i++)
-  {
-    if (strcmp(subs[i].topic, topic) == 0)
-    {
+  for (int i = 0; i < capacity_subs; i++) {
+    if (strcmp(subs[i].topic, topic) == 0) {
       send_to_subscriber(subs[i].pid, text);
     }
   }
 }
 
-static void parse_subs(char* text)
-{
+static void parse_subs(char* text) {
   char buffer[100];
-  strcpy(buffer, text);
+  snprintf(buffer, sizeof(buffer), "%s", text);
 
   char* token = strtok(buffer, ",");
   if (!token || strcmp(token, "subscribe") != 0) return;
@@ -130,19 +125,16 @@ static void parse_subs(char* text)
   int pid = atoi(pid_str);
   append_subscribe(pid, topic);
 
-  for (int i = 0; i < capacity_sms; i++)
-  {
-    if (strcmp(sms[i].topic, topic) == 0)
-    {
+  for (int i = 0; i < capacity_sms; i++) {
+    if (strcmp(sms[i].topic, topic) == 0) {
       send_to_subscriber(pid, sms[i].message);
     }
   }
 }
 
-static void parse_unsub(char* text)
-{
+static void parse_unsub(char* text) {
   char buffer[100];
-  strcpy(buffer, text);
+  snprintf(buffer, sizeof(buffer), "%s", text);
 
   char* token = strtok(buffer, ",");
 
@@ -157,75 +149,21 @@ static void parse_unsub(char* text)
   remove_subscribe(pid, topic);
 }
 
-static void process_message(message* msg)
-{
-  if (strncmp(msg->text, "send,", 5) == 0)
-  {
+static void process_message(message* msg) {
+  if (strncmp(msg->text, "send,", 5) == 0) {
     parse_pubs(msg->text);
-  }
-  else if (strncmp(msg->text, "subscribe,", 10) == 0)
-  {
+  } else if (strncmp(msg->text, "subscribe,", 10) == 0) {
     parse_subs(msg->text);
-  }
-  else if (strncmp(msg->text, "unsubscribe,", 12) == 0)
-  {
+  } else if (strncmp(msg->text, "unsubscribe,", 12) == 0) {
     parse_unsub(msg->text);
-  }
-  else
-  {
+  } else {
     fprintf(stderr, "Corrupted message %s\n", msg->text);
   }
 }
 
-void broker_mainloop()
-{
-  key_t key = ftok("queue", 500);
-  if (key == -1)
-  {
-    perror("ftok ");
-    exit(1);
-  }
-
-  queue_id = msgget(key, IPC_CREAT | IPC_EXCL | 0666);
-  if (queue_id == -1)
-  {
-    perror("Queue error ");
-    _exit(1);
-  }
-
-  printf("Брокер запущенб очередь создана: %d\n", queue_id);
-
-  subs = (sub*)calloc(sizeof(sub), capacity_subs);
-
-  pubs = (pub*)calloc(sizeof(pub), capacity_pubs);
-
-  sms = (sm*)calloc(sizeof(sm), capacity_sms);
-
-  message msg = {0};
-
-  long msgtyp = 1;
-
-  while (1)
-  {
-    ssize_t len = msgrcv(queue_id, (void*) &msg, sizeof(msg.text), msgtyp, 0);
-    if (len == -1)
-    {
-      perror("msgrcv ");
-      break;
-    }
-
-    msg.text[len] = '\0';
-
-    process_message(&msg);
-
-  }
-}
-
-void cleanup_broker()
-{
+void cleanup_broker() {
   for (int i = 0; i < capacity_subs; i++) free(subs[i].topic);
-  for (int i = 0; i < capacity_sms; i++)
-  {
+  for (int i = 0; i < capacity_sms; i++) {
     free(sms[i].topic);
     free(sms[i].message);
   }
@@ -233,4 +171,62 @@ void cleanup_broker()
   free(pubs);
   free(sms);
   if (queue_id != -1) msgctl(queue_id, IPC_RMID, NULL);
+}
+
+static void send_sigint_to_all() {
+  for (int i = 0; i < capacity_pubs; i++) {
+    kill(pubs[i].pid, SIGINT);
+  }
+  for (int i = 0; i < capacity_subs; i++) {
+    kill(subs[i].pid, SIGINT);
+  }
+}
+
+void broker_mainloop() {
+  key_t key = ftok("queue", 500);
+  if (key == -1) {
+    perror("ftok ");
+    exit(1);
+  }
+
+  queue_id = msgget(key, IPC_CREAT | IPC_EXCL | 0666);
+  if (queue_id == -1) {
+    perror("Queue error ");
+    _exit(1);
+  }
+
+  printf("Брокер запущен очередь создана: %d\n", queue_id);
+
+  struct sigaction sa;
+
+  sa.sa_handler = signal_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sigaction(SIGINT, &sa, NULL);
+
+  // subs = (sub*)calloc(sizeof(sub), capacity_subs);
+
+  // pubs = (pub*)calloc(sizeof(pub), capacity_pubs);
+
+  // sms = (sm*)calloc(sizeof(sm), capacity_sms);
+
+  message msg = {0};
+
+  long msgtyp = 1;
+
+  while (running) {
+    ssize_t len = msgrcv(queue_id, (void*)&msg, sizeof(msg.text), msgtyp, 0);
+    if (len == -1) {
+      // perror("msgrcv ");
+      break;
+    }
+
+    msg.text[len] = '\0';
+
+    process_message(&msg);
+  }
+
+  send_sigint_to_all();
+  cleanup_broker();
+  exit(0);
 }
--- module3/02/main.c
+++ module3/02/main.c
@@ -1,11 +1,12 @@
 #include <errno.h>
-#include <stdio.h>
-#include <sys/ipc.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <sys/msg.h>
 #include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+
 #include "broker.h"
 
 typedef enum role {
@@ -15,30 +16,40 @@ typedef enum role {
   subscruber,
 } role;
 
-static int get_queue_id()
-{
+static int get_queue_id() {
   key_t key = ftok("queue", 500);
-  if (key == -1) { perror("ftok"); exit(1);}
+  if (key == -1) {
+    perror("ftok");
+    exit(1);
+  }
   int qid = msgget(key, 0);
-  if (qid == -1) { perror("msgget "); exit(1); };
+  if (qid == -1) {
+    perror("msgget ");
+    exit(1);
+  };
   return qid;
 }
 
-volatile sig_atomic_t running = 1;
-void handler(int sig) { (void)sig; running = 0; }
+static volatile sig_atomic_t running = 1;
+void handler(int sig) {
+  (void)sig;
+  running = 0;
+}
 
-void publisher_loop(char* topic)
-{
+void publisher_loop(char* topic) {
   int qid = get_queue_id();
   int pid = getpid();
 
-  signal(SIGINT, handler);
+  struct sigaction sa;
+  sa.sa_handler = handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sigaction(SIGINT, &sa, NULL);
 
   printf("Издатель (тема: %s, PID=%d), Введите сообщение:\n", topic, pid);
 
   char buffer[256];
-  while (running)
-  {
+  while (running) {
     if (!fgets(buffer, sizeof(buffer), stdin)) break;
     buffer[strcspn(buffer, "\n")] = '\0';
     if (strlen(buffer) == 0) break;
@@ -47,8 +58,7 @@ void publisher_loop(char* topic)
     msg.type = 1;
     snprintf(msg.text, sizeof(msg.text), "send,%d,%s,%s", pid, topic, buffer);
 
-    if (msgsnd(qid, &msg, sizeof(msg.text), 0) == -1)
-    {
+    if (msgsnd(qid, &msg, sizeof(msg.text), 0) == -1) {
       perror("msgsnd ");
       break;
     }
@@ -57,35 +67,35 @@ void publisher_loop(char* topic)
   printf("Издатель завершён.\n");
 }
 
-void subscriber_loop(int num_topics, char** topics)
-{
+void subscriber_loop(int num_topics, char** topics) {
   int qid = get_queue_id();
   int pid = getpid();
 
-  for (int i = 0; i < num_topics; i++)
-  {
+  for (int i = 0; i < num_topics; i++) {
     message msg;
     msg.type = 1;
-    snprintf(msg.text, sizeof(msg.text), "subscribe,%d,%s",pid, topics[i]);
-    if (msgsnd(qid, &msg, sizeof(msg.text), 0) == -1)
-    {
+    snprintf(msg.text, sizeof(msg.text), "subscribe,%d,%s", pid, topics[i]);
+    if (msgsnd(qid, &msg, sizeof(msg.text), 0) == -1) {
       perror("msgsnd ");
       exit(1);
     }
     printf("Подписка на тему '%s' отправлена.\n", topics[i]);
   }
 
-  //volatile sig_atomic_t running = 1;
-  //void handler(int sig) { (void)sig; running = 0; }
-  signal(SIGINT, handler);
+  struct sigaction sa;
+  sa.sa_handler = handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sigaction(SIGINT, &sa, NULL);
+
+  // volatile sig_atomic_t running = 1;
+  // void handler(int sig) { (void)sig; running = 0; }
 
   message msg;
   printf("Подписчик (PID=%d) ожидает сообщения...\n", pid);
-  while (running)
-  {
+  while (running) {
     ssize_t len = msgrcv(qid, &msg, sizeof(msg.text), pid, 0);
-    if (len == -1)
-    {
+    if (len == -1) {
       perror("msgrcv ");
       break;
     }
@@ -93,13 +103,11 @@ void subscriber_loop(int num_topics, char** topics)
     printf("Получинл: %s\n", msg.text);
   }
 
-  for (int i = 0; i < num_topics; i++)
-  {
+  for (int i = 0; i < num_topics; i++) {
     message msg;
     msg.type = 1;
     snprintf(msg.text, sizeof(msg.text), "unsubscribe,%d,%s", pid, topics[i]);
-    if (msgsnd(qid, &msg, sizeof(msg.text), 0) == -1)
-    {
+    if (msgsnd(qid, &msg, sizeof(msg.text), 0) == -1) {
       perror("msgsnd ");
     }
     printf("Отписка от темы '%s' отправлена.\n", topics[i]);
@@ -107,8 +115,7 @@ void subscriber_loop(int num_topics, char** topics)
   printf("Подписчик завершён.\n");
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
   int opt;
   int queue_id = 0;
   role _role = nothing;
@@ -116,10 +123,8 @@ int main(int argc, char* argv[])
   char** topics = NULL;
   int num_topics = 0;
 
-  while ((opt = getopt(argc, argv, "bp:s")) != -1)
-  {
-    switch (opt)
-    {
+  while ((opt = getopt(argc, argv, "bp:s")) != -1) {
+    switch (opt) {
       case 'b':
         _role = broker;
         break;
@@ -134,29 +139,24 @@ int main(int argc, char* argv[])
         errno = 13;
         perror("Недопустимый флаг (ввод аргументов только с флагом) ");
         _exit(1);
-
     }
   }
 
-  if (_role == subscruber)
-  {
+  if (_role == subscruber) {
     num_topics = argc - optind;
     topics = &argv[optind];
-    if (num_topics == 0)
-    {
+    if (num_topics == 0) {
       fprintf(stderr, "Нужга хотя бы одна тема\n");
       exit(1);
     }
   }
 
-  switch (_role)
-  {
+  switch (_role) {
     case broker:
       broker_mainloop();
       break;
     case publisher:
-      if (!topic)
-      {
+      if (!topic) {
         fprintf(stderr, "Нужно указать тему!\n");
         exit(1);
       }
