7b560f421f7adee21741b0925e9cb50527474e95
danil <kornachenkodanil@gmail.com>
2026-08-13 19:55:02 +0700

    module3 cross01 : client

--- module3/cross01/echo_client.c
+++ module3/cross01/echo_client.c
@@ -1,15 +1,36 @@
-#include <signal.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
 int sockfd = -1;
 int running = 1;
+struct sockaddr_in addr = {0};
+int client_port = -1;
 
 static void signal_handler(int sig) {
   (void)sig;
   running = 0;
+  char buffer[1024] = {0};
+  struct udphdr* udp = (struct udphdr*)buffer;
+  udp->source = htons(client_port);
+  udp->dest = htons(5000);
+  udp->len = htons(sizeof(struct udphdr) + 4);
+  udp->check = 0;
+  strcpy(buffer + sizeof(struct udphdr), "EXIT");
+  sendto(sockfd, buffer, sizeof(struct udphdr) + 4, 0, (struct sockaddr*)&addr,
+         sizeof(addr));
+  close(sockfd);
+  exit(0);
 }
 
 int main(void) {
@@ -22,16 +43,98 @@ int main(void) {
 
   signal(SIGINT, signal_handler);
 
-  struct sockaddr_in addr = {0};
+  // struct sockaddr_in addr = {0};
 
   addr.sin_family = AF_INET;
+  addr.sin_port = htons(5000);
+  addr.sin_addr.s_addr = INADDR_ANY;
+  socklen_t len = sizeof(addr);
 
-  if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    perror("bind ");
-    exit(1);
+  srand(time(NULL));
+
+  client_port = (rand() % 40000) + 20000;
+
+  char buffer[1024] = {0};
+
+  while (running) {
+    char* message = NULL;
+    size_t bufsize = 0;
+    ssize_t length = getline(&message, &bufsize, stdin);
+    if (length == -1) {
+      // ошибка или EOF
+      free(message);
+      break;
+    }
+    // len включает '\n', если он есть
+    if (length > 0 && message[length - 1] == '\n') {
+      message[length - 1] = '\0';
+      length--;
+    }
+
+    struct udphdr* udphdr = (struct udphdr*)buffer;
+
+    udphdr->source = htons(client_port);
+    udphdr->dest = htons(5000);
+    udphdr->len = htons(sizeof(struct udphdr) + strlen(message));
+    udphdr->check = 0;
+
+    char* data = buffer + sizeof(struct udphdr);
+    strcpy(data, message);
+
+    ssize_t sent =
+        sendto(sockfd, buffer, sizeof(struct udphdr) + strlen(message), 0,
+               (struct sockaddr*)&addr, len);
+
+    if (sent == -1) {
+      perror("sendto ");
+    } else {
+      printf("Отправлено\n");
+    }
+
+    uint16_t src_port = 0;
+    uint16_t dest_port = 0;
+    struct iphdr* iphdr = NULL;
+    struct udphdr* recvudp = NULL;
+    int ip_size = 0;
+    int n = -1;
+    while (src_port != 5000 && dest_port != client_port) {
+      n = recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&addr, &len);
+
+      if (n == -1) {
+        perror("recvfrom ");
+        continue;
+      }
+
+      iphdr = (struct iphdr*)buffer;
+
+      if (iphdr->protocol != IPPROTO_UDP) continue;
+
+      ip_size = iphdr->ihl * 4;
+
+      recvudp = (struct udphdr*)(buffer + ip_size);
+
+      src_port = ntohs(recvudp->source);
+      dest_port = ntohs(recvudp->dest);
+    }
+    if (iphdr == NULL) continue;
+    if (recvudp == NULL) continue;
+
+    if (iphdr->saddr == iphdr->daddr && recvudp->source == recvudp->dest) {
+      printf("Один и тот же адрес:(\n");
+      continue;
+    }
+
+    char* recvdata = buffer + sizeof(struct udphdr) + ip_size;
+
+    int data_len = n - ip_size - sizeof(struct udphdr);
+
+    printf("response: %.*s\n", data_len, recvdata);
+    free(message);
+    client_port = -1;
+    src_port = -1;
   }
 
-
+  close(sockfd);
 
   return 0;
 }
--- module3/cross01/echo_server.c
+++ module3/cross01/echo_server.c
@@ -1,22 +1,99 @@
-#include <netinet/in.h>
-#include <stdint.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/udp.h>
+#include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 65512
+#define MAX_CLIENTS 100
 
 int sockfd = -1;
 int running = 1;
 
+typedef struct cli {
+  struct sockaddr_in addr;
+  int count;
+} client_t;
+
+int clients_count = 0;
+
+client_t clients[MAX_CLIENTS] = {0};
+
 static void signal_handler(int sig) {
   (void)sig;
   running = 0;
+  close(sockfd);
+  exit(0);
+}
+
+static client_t* add_client(client_t client) {
+  for (int i = 0; i < clients_count; i++) {
+    if (client.addr.sin_port == clients[i].addr.sin_port &&
+        client.addr.sin_addr.s_addr == clients[i].addr.sin_addr.s_addr) {
+      return &clients[i];
+    }
+  }
+  if (clients_count >= MAX_CLIENTS) {
+    fprintf(stderr, "Слишком много клиентов\n");
+    return NULL;
+  }
+  clients[clients_count] = client;
+  clients_count++;
+  return &clients[clients_count - 1];
+}
+
+static int del_client(client_t client) {
+  for (int i = 0; i < clients_count; i++) {
+    if (client.addr.sin_port == clients[i].addr.sin_port &&
+        client.addr.sin_addr.s_addr == clients[i].addr.sin_addr.s_addr) {
+      clients[i].addr.sin_addr.s_addr = 0;
+      clients[i].addr.sin_port = 0;
+      clients[i].count = 0;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+static void send_client(client_t client, char* message, int size) {
+  if (size >= 5) {
+    if (strncmp(message, "EXIT", 5) == 0) {
+      int res = del_client(client);
+      if (res) {
+        printf("Client exit\n");
+      }
+      return;
+    }
+  }
+
+  client_t* cli = add_client(client);
+
+  char mess[1024] = {0};
+
+  cli->count++;
+
+  snprintf(mess, sizeof(mess), "%s %d", message, cli->count);
+
+  char buffer[1024] = {0};
+
+  struct udphdr* udphdr = (struct udphdr*)buffer;
+
+  udphdr->dest = cli->addr.sin_port;
+  udphdr->source = htons(5000);
+  udphdr->check = 0;
+  udphdr->len = htons(sizeof(struct udphdr) + strlen(mess));
+
+  char* data = buffer + sizeof(struct udphdr);
+  strcpy(data, mess);
+
+  sendto(sockfd, buffer, sizeof(struct udphdr) + strlen(mess), 0,
+         (struct sockaddr*)&cli->addr, sizeof(cli->addr));
 }
 
 int main(void) {
@@ -38,12 +115,15 @@ int main(void) {
   char buffer[BUFFER_SIZE] = {0};
 
   while (running) {
-    int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&addr, &socklen);
+    int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&addr,
+                     &socklen);
 
     if (n == -1) {
       perror("recvfrom ");
       break;
     }
+
+    buffer[n] = '\0';
 
     struct iphdr* iphdr = (struct iphdr*)buffer;
 
@@ -56,6 +136,13 @@ int main(void) {
     uint16_t src_port = ntohs(udphdr->source);
     uint16_t dest_port = ntohs(udphdr->dest);
 
+    if (dest_port != 5000) continue;
+
+    if (iphdr->saddr == iphdr->daddr && udphdr->source == udphdr->dest) {
+      printf("Один и тот же адрес :(\n");
+      continue;
+    }
+
     char src_ip[INET_ADDRSTRLEN], dest_ip[INET_ADDRSTRLEN];
 
     inet_ntop(AF_INET, &iphdr->saddr, src_ip, INET_ADDRSTRLEN);
@@ -64,8 +151,18 @@ int main(void) {
     char* data = buffer + sizeof(struct udphdr) + ip_size;
     int data_len = n - ip_size - sizeof(struct udphdr);
 
-    printf("Source:\t%s:%u\nDest:\t%s:%u\nData: %s\nData Length: %d", src_ip, src_port, dest_ip, dest_port, data, data_len);
+    printf("Source:\t%s:%u\nDest:\t%s:%u\nData: %.*s\nData Length: %d\n",
+           src_ip, src_port, dest_ip, dest_port, data_len, data, data_len);
 
+    // TODO: sendto client
+    struct sockaddr_in cli_addr = {0};
+    cli_addr.sin_port = udphdr->source;
+    cli_addr.sin_addr.s_addr = iphdr->saddr;
+    cli_addr.sin_family = AF_INET;
+    client_t cli = {0};
+    cli.addr = cli_addr;
+
+    send_client(cli, data, n - ip_size - sizeof(struct udphdr));
   }
 
   close(sockfd);
